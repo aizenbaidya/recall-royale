805befd4be53f81fc96b372d1fa480f6633c7d3b
Parth Jain <parthgmjain@gmail.com>
2024-10-11 10:14:27 -0700

    fixing routing

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
- 
 @app.route('/')
 def home():
     return render_template('home.html')
@@ -12,7 +11,8 @@ def about():
     return render_template('about.html') 
 
 @app.route('/play')
-def about():
+def play_game():
     return render_template('play.html')   
+
 if __name__ == '__main__':
     app.run(debug=True)
